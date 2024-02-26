8bfa59046122e830063d00d7610299e309bcdc09
Nick Breland <npbreland@tutanota.com>
2024-02-26 15:19:54 +0000

    refactor: consistent spacing in arithmetic expressions

--- bash-stopwatch.sh
+++ bash-stopwatch.sh
@@ -5,13 +5,13 @@ seconds=0
 function print()
 {
     if [ $seconds -gt 3600 ]; then
-        local hours=$((seconds/3600))
-        local minutes=$((seconds/60))
-        local seconds_beyond=$((seconds%60))
+        local hours=$((seconds / 3600))
+        local minutes=$((seconds / 60))
+        local seconds_beyond=$((seconds % 60))
         printf "\n$hours hours, $minutes minutes, and $seconds_beyond seconds elapsed"
     elif [ $seconds -gt 60 ]; then
-        local minutes=$((seconds/60))
-        local seconds_beyond=$((seconds%60))
+        local minutes=$((seconds / 60))
+        local seconds_beyond=$((seconds % 60))
         printf "\n$minutes minutes and $seconds_beyond seconds elapsed"
     else
         printf "\n$seconds seconds elapsed"
@@ -29,7 +29,7 @@ trap "print_and_exit" SIGINT
 while [ true ]
 do
     sleep 1
-    seconds=$((seconds+1))
+    seconds=$((seconds + 1))
     if [ $((seconds % 5)) -eq 0 ]; then
         print
     fi
