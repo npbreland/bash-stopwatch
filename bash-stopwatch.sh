c3f9a7f5ed4955d94e2419ae09eb322b4b672147
Nick Breland <npbreland@tutanota.com>
2024-02-26 15:17:01 +0000

    fix: seconds beyond

--- bash-stopwatch.sh
+++ bash-stopwatch.sh
@@ -7,10 +7,12 @@ function print()
     if [ $seconds -gt 3600 ]; then
         local hours=$((seconds/3600))
         local minutes=$((seconds/60))
-        printf "\n$hours hours, $minutes minutes, and $seconds seconds elapsed"
+        local seconds_beyond=$((seconds%60))
+        printf "\n$hours hours, $minutes minutes, and $seconds_beyond seconds elapsed"
     elif [ $seconds -gt 60 ]; then
         local minutes=$((seconds/60))
-        printf "\n$minutes minutes and $seconds seconds elapsed"
+        local seconds_beyond=$((seconds%60))
+        printf "\n$minutes minutes and $seconds_beyond seconds elapsed"
     else
         printf "\n$seconds seconds elapsed"
     fi
