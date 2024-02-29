77b84f0760cfe45b3a4665048c7a924cdea13e3d
Nick Breland <npbreland@tutanota.com>
2024-02-29 08:14:24 +0000

    refactor: stdout/stderr handling

--- bash-stopwatch.sh
+++ bash-stopwatch.sh
@@ -2,26 +2,27 @@
 
 seconds=0
 
-function print()
+function elapsed_text()
 {
     if [ $seconds -gt 3600 ]; then
         local hours=$((seconds / 3600))
         local minutes=$((seconds / 60))
         local minutes_beyond=$((minutes % 60))
         local seconds_beyond=$((seconds % 60))
-        printf "\n$hours hours, $minutes_beyond minutes, and $seconds_beyond seconds elapsed"
+        echo "\n$hours hours, $minutes_beyond minutes, and $seconds_beyond seconds elapsed"
     elif [ $seconds -gt 60 ]; then
         local minutes=$((seconds / 60))
         local seconds_beyond=$((seconds % 60))
-        printf "\n$minutes minutes and $seconds_beyond seconds elapsed"
+        echo "\n$minutes minutes and $seconds_beyond seconds elapsed"
     else
-        printf "\n$seconds seconds elapsed"
+        echo "\n$seconds seconds elapsed"
     fi
 }
 
 function print_and_exit()
 {
-    print
+    printf "$(elapsed_text)" >&2 
+    echo $seconds
     exit
 }
 
@@ -32,6 +33,6 @@ do
     sleep 1
     seconds=$((seconds + 1))
     if [ $((seconds % 5)) -eq 0 ]; then
-        print
+        printf "$(elapsed_text)"  >&2 
     fi
 done
